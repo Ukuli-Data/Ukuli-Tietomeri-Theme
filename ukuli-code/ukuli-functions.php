96d38f524cbe39d6c3b2f1fe1ba7e956e4e589e5
Tomi Toivio <tomi.toivio@ukuli.fi>
2020-08-23 05:20:57 +0300

    chart.js, vue.js etc

---
+++ ukuli-code/ukuli-functions.php
@@ -0,0 +1,6 @@
+<?php
+/* Load classes */
+
+require get_template_directory() . 'ukuli-code/ukuli-iot-class.php';
+require get_template_directory() . 'ukuli-code/ukuli-iota-class.php';
+require get_template_directory() . 'ukuli-code/ukuli-user-class.php';
