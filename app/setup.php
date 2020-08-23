3f2f985807132234c10dfc34e113e74edccfaf6e
Tomi Toivio <tomi.toivio@ukuli.fi>
2020-08-23 05:30:23 +0300

    chart.js, vue.js etc

--- app/setup.php
+++ app/setup.php
@@ -13,6 +13,14 @@ use Roots\Sage\Template\BladeProvider;
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
+    /* Ukuli Data styles */
+    wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
+    wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
+    wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
+    /* Ukuli Data scripts */
+    wp_enqueue_script('ukuli-chart.js', asset_path('scripts/ukuli.js/main.js'), ['jquery'], null, true);
+    wp_enqueue_script('ukuli-jquery.js', asset_path('scripts/ukuli.js/main.js'), ['jquery'], null, true);
+    wp_enqueue_script('ukuli-vue.js', asset_path('scripts/ukuli.js/main.js'), ['jquery'], null, true);
 
     if (is_single() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,8 +3,4 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php wp_head() @endphp
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.12/vue.min.js" integrity="sha512-BKbSR+cfyxLdMAsE0naLReFSLg8/pjbgfxHh/k/kUC82Hy7r6HtR5hLhobaln2gcTvzkyyehrdREdjpsQwy2Jw==" crossorigin="anonymous"></script>
-  <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
-  <script src="https://kit.fontawesome.com/2ab0b091bd.js" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" integrity="sha512-s+xg36jbIujB2S2VKfpGmlC3T5V2TF3lY48DX7u2r9XzGzgPsa6wTpOQA7J9iffvdeBN0q9tKzRxVxw1JviZPg==" crossorigin="anonymous"></script>
 </head>
