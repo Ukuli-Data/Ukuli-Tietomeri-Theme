95b7c977956fb86b2ed987520f4555e93821458d
Tomi Toivio <tomi.toivio@ukuli.fi>
2020-08-26 06:31:00 +0300

    More templates

---
+++ resources/views/template-custom-tietomeri-frontpage.blade.php
@@ -0,0 +1,12 @@
+{{--
+  Template Name: Tietomeri Custom Frontpage Template
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php the_post() @endphp
+    @include('partials.page-header')
+    @include('partials.content-page')
+  @endwhile
+@endsection
